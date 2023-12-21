556148caa61f92f1bc08eedba7baba375f9c8263
Dan Brandt <dan.p.brandt@gmail.com>
2023-12-21 16:30:21 -0600

    first push mostly done

---
+++ README.md
@@ -0,0 +1,9 @@
+Overview
+This JavaScript application empowers users, especially employees handling sensitive data, to generate secure and random passwords based on their selected criteria. It offers a dynamic, responsive user interface that functions seamlessly across various screen sizes.
+
+Features
+Criteria-Based Generation: Users can generate passwords based on specific criteria, including length and character types (lowercase, uppercase, numeric, and special characters).
+Responsive UI: The application adapts to different screen sizes, ensuring a consistent user experience.
+Customizable Length: Password length can be set between 8 and 128 characters.
+Character Type Selection: Users have the option to include or exclude specific character types in their password.
+Validation: The application validates user inputs to ensure at least one character type is selected.
---
+++ assets/scripttest.js
@@ -0,0 +1,56 @@
+// Function to generate password
+
+function generatePassword() {
+    var length = prompt("How many characters would you like your password to be? (8-128 characters)"); // prompt user for password length and store it in 'length
+    while (length < 8 || length > 128) {  // Loops until length meets criteria
+        length = prompt("Length must be 8-128 characters. How many characters would you like your password to be?");
+    }
+// confirm dialogs to include different character types in the password
+    var includeUppercase = confirm("WOULD YOU LIKE TO INCLUDE UPPERCASE LETTERS?");
+    var includeLowercase = confirm("Would you like to include lowercase letters?");
+    var includeNumbers = confirm("Would you like to include numb3rs?");
+    var includeSymbols = confirm("Would you like to include spec!al ch@racter$?");
+
+// if user doesnt choose one option, make them choose again. loops
+    while (!includeUppercase && !includeLowercase && !includeNumbers && !includeSymbols) {
+        alert("PLEASE! Select at least one option! I need SOMETHING to work with...");
+        includeUppercase = confirm("WOULD YOU LIKE TO INCLUDE UPPERCASE LETTERS?");
+        includeLowercase = confirm("Would you like to include lowercase letters?");
+        includeNumbers = confirm("Would you like to include numb3rs?");
+        includeSymbols = confirm("Would you like to include spec!al ch@rcter$?");
+    }
+// different string chacters the user can choose from. 
+    var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    var numberChars = "0123456789";
+    var symbolChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+// emptied string to combine users chosen character types
+    var allChars = "";
+    if (includeUppercase) allChars += uppercaseChars;
+    if (includeLowercase) allChars += lowercaseChars;
+    if (includeNumbers) allChars += numberChars;
+    if (includeSymbols) allChars += symbolChars;
+// empty string for the password
+    var password = "";
+    for (var i = 0; i < length; i++) {
+        password += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    }
+// generates the password
+    return password;
+}
+
+
+// the function to write the generated password to the "input" element
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// selects the generate button element by its ID
+// Add event listener to generate button
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
